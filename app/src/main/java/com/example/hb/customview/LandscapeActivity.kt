0da350c27433f8abbb72a310aafcdbd6127a19be
cesmai <cecilemaigrot@yahoo.fr>
2018-11-19 14:52:40 +0100

    Add Landscape layout

---
+++ app/src/main/java/com/example/hb/customview/LandscapeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hb.customview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LandscapeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landscape)
+    }
+}
